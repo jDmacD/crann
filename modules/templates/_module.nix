f145dbcc8842632c1acea4aa9ce315c41d4fe4ac
jDmacD <jmacdoo@gmail.com>
2026-08-15 17:28:21 +0100

    build(templates): add homeManager specific options

--- modules/templates/_module.nix
+++ modules/templates/_module.nix
@@ -13,7 +13,54 @@
     {
       options.crann.{{ name }} = {
         enable = lib.mkEnableOption "{{ name }}";
+{% if type == 'homeManager' %}
+        packages = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          default = [ pkgs.hello ];
+          defaultText = lib.literalExpression "[ pkgs.hello ]";
+          description = "The {{ name }} tools to install. Replaces crann's default set.";
+        };
+
+        extraPackages = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          default = [ ];
+          example = lib.literalExpression "[ pkgs.hello ]";
+          description = "Extra packages installed alongside `packages`.";
+        };
+
+        # {{ name }} = {
+        #   enable = lib.mkOption {
+        #     type = lib.types.bool;
+        #     default = true;
+        #     description = "Whether to install and configure {{ name }}.";
+        #   };
+
+        #   package = lib.mkOption {
+        #     type = lib.types.package;
+        #     default = pkgs.{{ name }};
+        #     defaultText = lib.literalExpression "pkgs.{{ name }}";
+        #     description = "The {{ name }} package to use.";
+        #   };
+
+        #  extraSettings = lib.mkOption {
+        #    type = lib.types.attrsOf lib.types.anything;
+        #    default = { };
+        #    description = "Extra {{ name }} settings merged over crann's defaults.";
+        #  };
+        # };
+{% endif %}
       };
-      config = lib.mkIf cfg.enable { };
+      config = lib.mkIf cfg.enable {
+{% if type == 'homeManager' %}
+        home.packages = cfg.packages ++ cfg.extraPackages;
+
+        # programs.{{ name }} = {
+        #   enable = cfg.{{ name }}.enable;
+        #   package = cfg.{{ name }}.package;
+        #   settings = cfg.{{ name }}.extraSettings;
+        # };
+
+{% endif %}
+      };
     };
 }
