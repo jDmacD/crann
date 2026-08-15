23c1dc6abb79943e2dcf4b6d1f86237f7b8ab9b9
jDmacD <jmacdoo@gmail.com>
2026-08-15 17:46:57 +0100

    build(template): add nixos module boilerplate

--- modules/templates/_module.nix
+++ modules/templates/_module.nix
@@ -48,6 +48,39 @@
         #    description = "Extra {{ name }} settings merged over crann's defaults.";
         #  };
         # };
+{% elif type == 'nixos' %}
+        packages = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          default = [ pkgs.hello ];
+          defaultText = lib.literalExpression "[ pkgs.hello ]";
+          description = "The {{ name }} tools to install. Replaces crann's default set.";
+        };
+
+        extraPackages = lib.mkOption {
+          type = lib.types.listOf lib.types.package;
+          default = [ ];
+          example = lib.literalExpression "[ pkgs.hello ]";
+          description = "Extra packages installed alongside `packages`.";
+        };
+
+        # package = lib.mkOption {
+        #   type = lib.types.package;
+        #   default = pkgs.{{ name }};
+        #   defaultText = lib.literalExpression "pkgs.{{ name }}";
+        #   description = "The {{ name }} package to use.";
+        # };
+
+        # extraSettings = lib.mkOption {
+        #   type = lib.types.attrsOf lib.types.anything;
+        #   default = { };
+        #   description = "Extra {{ name }} settings merged over crann's defaults.";
+        # };
+
+        # user = lib.mkOption {
+        #   type = lib.types.str;
+        #   default = "{{ name }}";
+        #   description = "User account under which {{ name }} runs.";
+        # };
 {% endif %}
       };
       config = lib.mkIf cfg.enable {
@@ -58,6 +91,15 @@
         #   enable = cfg.{{ name }}.enable;
         #   package = cfg.{{ name }}.package;
         #   settings = cfg.{{ name }}.extraSettings;
+        # };
+
+{% elif type == 'nixos' %}
+        environment.systemPackages = cfg.packages ++ cfg.extraPackages;
+
+        # services.{{ name }} = {
+        #   enable = cfg.enable;
+        #   package = cfg.package;
+        #   settings = cfg.extraSettings;
         # };
 
 {% endif %}
