9a7c2c85241e4aae6eeb6aed67a134e35b8d01c8
jDmacD <jmacdoo@gmail.com>
2026-08-24 16:33:08 +0100

    fix(builder-user): missing lib

--- modules/nixos/builder-user.nix
+++ modules/nixos/builder-user.nix
@@ -3,6 +3,7 @@
   flake.modules.nixos.builderUser =
     {
       config,
+      lib,
       ...
     }:
     let
@@ -10,22 +11,22 @@
     in
     {
       options.crann.builderUser = {
-
+        enable = lib.mkEnableOption "builderUser";
       };
 
       config = lib.mkIf cfg.enable {
 
-  users.users.builder = {
-    isNormalUser = true;
-    createHome = false;
-    ignoreShellProgramCheck = true;
-    group = "builder";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnim/f3xwmFw/DB9zeHtQSr9i2uKxwsiXkEgE2FdFcY root@picard"
-    ];
-  };
-  users.groups.builder = { };
-  nix.settings.trusted-users = [ "builder" ];
+        users.users.builder = {
+          isNormalUser = true;
+          createHome = false;
+          ignoreShellProgramCheck = true;
+          group = "builder";
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnim/f3xwmFw/DB9zeHtQSr9i2uKxwsiXkEgE2FdFcY root@picard"
+          ];
+        };
+        users.groups.builder = { };
+        nix.settings.trusted-users = [ "builder" ];
 
       };
     };
